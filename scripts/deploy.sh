f30fd9f6d8b3870ecc16678c275c883f414d11f7
maccac <maccac@not.an.email.com>
2019-03-11 09:27:57 +1100

    Configuring the Docker release
    - Deploy Tags when a TAG is set
    - Deploy script for branch 23 for testing
    - Removed some of the less useful tagging like commit number

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 TAG=test-deploy-branch
 REPO=tyro/rabbit-amazon-bridge
+TAG=$1
 
 echo "$DOCKERHUB_PASS" | docker login -u "$DOCKERHUB_USER" --password-stdin
-docker build -f Dockerfile.deploy -t $REPO:$TRAVIS_COMMIT . --build-arg BUILD_IMAGE=tyro/rabbit-amazon-bridge-build:latest
-docker tag $REPO:$TRAVIS_COMMIT $REPO:$TAG
-docker tag $REPO:$TRAVIS_COMMIT $REPO:travis-$TRAVIS_BUILD_NUMBER
+docker build -f Dockerfile.deploy -t $REPO:$TAG . --build-arg BUILD_IMAGE=tyro/rabbit-amazon-bridge-build:latest
 docker push $REPO
