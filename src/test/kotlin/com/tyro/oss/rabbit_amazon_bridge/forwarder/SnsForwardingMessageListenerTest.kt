d34cde8d29c16dba524ad5e22cde885221892de3
Mahendar Selvakumar <mselvakumar@tyro.com>
2021-03-01 15:54:46 +1000

    LEN-1840: Added 'type' messge attribute to messages sent to SNS if the rabbit payload contains type

--- src/test/kotlin/com/tyro/oss/rabbit_amazon_bridge/forwarder/SnsForwardingMessageListenerTest.kt
+++ src/test/kotlin/com/tyro/oss/rabbit_amazon_bridge/forwarder/SnsForwardingMessageListenerTest.kt
@@ -16,10 +16,12 @@
 
 package com.tyro.oss.rabbit_amazon_bridge.forwarder
 
-
 import com.nhaarman.mockito_kotlin.argumentCaptor
 import com.tyro.oss.rabbit_amazon_bridge.messagetransformer.MessageTransformer
 import com.tyro.oss.randomdata.RandomString.randomString
+import io.cloudevents.json.Json
+import io.cloudevents.v1.CloudEventBuilder
+import io.cloudevents.v1.CloudEventImpl
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -32,6 +34,8 @@ import org.springframework.amqp.core.Message
 import org.springframework.amqp.core.MessageProperties
 import org.springframework.cloud.aws.messaging.core.NotificationMessagingTemplate
 import org.springframework.messaging.MessagingException
+import java.net.URI
+import java.time.ZonedDateTime
 import kotlin.test.assertFailsWith
 
 @RunWith(MockitoJUnitRunner::class)
@@ -46,14 +50,14 @@ class SnsForwardingMessageListenerTest {
     @Test
     fun `should scrub the rabbit message before sending to aws sqs`() {
         val topicName = "topic-name"
-        val rabbitMessage = "Some message"
+        val rabbitMessage = "{\"test\": \"data\"}"
         val transformedRabbitMessage = "transformed message"
         val message = RabbitMessage(rabbitMessage.toByteArray(), MessageProperties())
         val expectedSqsMessage = AWSStringMessageBuilder.withPayload(transformedRabbitMessage).build()
 
         `when`(messageTransformer.transform(rabbitMessage)).thenReturn(transformedRabbitMessage)
 
-        val sqsForwardingMessageListener = SnsForwardingMessageListener(topicName, topicNotificationMessagingTemplate,  messageTransformer)
+        val sqsForwardingMessageListener = SnsForwardingMessageListener(topicName, topicNotificationMessagingTemplate, messageTransformer)
         sqsForwardingMessageListener.onMessage(message)
 
         val captor = argumentCaptor<org.springframework.messaging.Message<String>>()
@@ -63,17 +67,57 @@ class SnsForwardingMessageListenerTest {
     }
 
     @Test
+    fun `should copy type to message attributes in SNS message if it is available in the rabbit message`() {
+        val topicName = "topic-name"
+        val expectedType = randomString()
+        val expectedDataContentType = "application/json"
+        val expectedId = randomString()
+        val expectedDateTime = ZonedDateTime.now()
+        val expectedSource = "http://www.test.com"
+
+        val cloudEvent: CloudEventImpl<String> =
+            CloudEventBuilder.builder<String>()
+                .withId(expectedId)
+                .withType(expectedType)
+                .withSource(URI.create(expectedSource))
+                .withTime(expectedDateTime)
+                .withDataContentType(expectedDataContentType)
+                .withData("{ \"test\" : \"data\"}")
+                .build()
+
+        val payload = Json.encode(cloudEvent)
+
+        val transformedRabbitMessage = "transformed message"
+        val message = RabbitMessage(payload.toByteArray(), MessageProperties())
+
+        `when`(messageTransformer.transform(payload)).thenReturn(transformedRabbitMessage)
+
+        val sqsForwardingMessageListener = SnsForwardingMessageListener(topicName, topicNotificationMessagingTemplate, messageTransformer)
+        sqsForwardingMessageListener.onMessage(message)
+
+        val captor = argumentCaptor<org.springframework.messaging.Message<String>>()
+        Mockito.verify(topicNotificationMessagingTemplate).send(Mockito.eq(topicName), captor.capture())
+
+        val expectedMessage = captor.firstValue
+
+        assertThat(expectedMessage.headers.containsKey("type")).isTrue()
+        assertThat(expectedMessage.headers["type"]).isEqualTo(expectedType)
+
+        assertThat(expectedMessage.payload).isEqualTo("transformed message")
+    }
+
+    @Test
     fun `should escalate exception when MessagingException thrown`() {
 
         val sqsForwardingMessageListener = SnsForwardingMessageListener(randomString(), topicNotificationMessagingTemplate, messageTransformer)
         Mockito.doThrow(MessagingException("AWS failed!!!"))
-                .`when`(topicNotificationMessagingTemplate)
-                .send(anyString(), Mockito.any(org.springframework.messaging.Message::class.java))
+            .`when`(topicNotificationMessagingTemplate)
+            .send(anyString(), Mockito.any(org.springframework.messaging.Message::class.java))
 
-        Mockito.`when`(messageTransformer.transform(Mockito.anyString())).thenReturn(randomString())
+        `when`(messageTransformer.transform(anyString())).thenReturn(randomString())
 
         assertFailsWith<MessagingException> {
-            sqsForwardingMessageListener.onMessage(Message(randomString().toByteArray(), MessageProperties()))
+            sqsForwardingMessageListener.onMessage(Message("{}".toByteArray(), MessageProperties()))
         }
     }
 }
